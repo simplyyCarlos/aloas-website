7123ef54e1dddf300486af9a690a52958cdc7ee2
ElPountie <khalis.rabehi@etu.univ-lyon1.fr>
2023-07-07 18:19:50 +0200

    init : start articles page

--- aloas-website/src/components/NavBar.vue
+++ aloas-website/src/components/NavBar.vue
@@ -2,6 +2,7 @@
     <div class="navbar">
         <ul>
             <li>Accueil</li>
+            <li><router-link to="/articles">Articles</router-link></li>
             <li>Évenements</li>
             <li>A propos</li>
             <li>Nous contacter</li>
